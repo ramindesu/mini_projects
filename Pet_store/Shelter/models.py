3febaac05b73004331cb62443dd0c0d7c7ba33b9
ramin <ramindesu88@gmail.com>
2025-11-06 21:14:02 +0330

    ADD(pet model)

--- Pet_store/Shelter/models.py
+++ Pet_store/Shelter/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator , RegexValidator
 # Create your models here.
+class Pet(models.Model):
+    name = models.CharField(max_length=60)
+    species= models.CharField(max_length=60)
+    age = models.PositiveSmallIntegerField(validators=[MinValueValidator(1)])
+    descriptions = models.TextField(null=True,blank= True)
+    image = models.ImageField()
+    available = models.BooleanField(default=True)
+    adopted = models.DateTimeField(auto_now_add=True)
+    siblings = models.ForeignKey('self',on_delete=models.SET_NULL,null= True,blank=True)
+    
