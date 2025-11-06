2e1330d50f6a86f212f96cf86c5c0f8bc8539c79
ramin <ramindesu88@gmail.com>
2025-11-06 21:57:40 +0330

    ADMIN REGISTER

--- Pet_store/Shelter/admin.py
+++ Pet_store/Shelter/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Pet , Owner
 # Register your models here.
+@admin.register(Owner)
+class AdminOwner(admin.ModelAdmin):
+    pass
+@admin.register(Pet)
+class AdminPet(admin.ModelAdmin):
+    pass
