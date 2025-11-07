6903aaad58ac6eb25ddf74683ed439aa2c575bc7
ramin <ramindesu88@gmail.com>
2025-11-07 10:19:18 +0330

    UPDATE(admin fields)

--- Pet_store/Shelter/admin.py
+++ Pet_store/Shelter/admin.py
@@ -4,10 +4,14 @@ from .models import Pet , Owner
 
 @admin.register(Owner)
 class AdminOwner(admin.ModelAdmin):
-    search_fields = ('first_name',)
-    list_filter = ('last_name' , )
-    fields = ('first_name' , 'last_name')
+    search_fields = ('first_name', 'last_name', 'phone')
+    list_filter = ('last_name',)
+    list_display = ('first_name', 'last_name', 'phone', 'request_time')
+    readonly_fields = ('request_time',)
+
+
 @admin.register(Pet)
 class AdminPet(admin.ModelAdmin):
-
-    search_fields = ('name',)
+    search_fields = ('name', 'species', 'owner__first_name', 'owner__last_name')
+    list_display = ('name', 'species', 'age', 'available', 'owner')
+    list_filter = ('available', 'species')
