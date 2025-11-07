acd999f706e80d5ec154e56e20065086d5fd0e85
ramin <ramindesu88@gmail.com>
2025-11-07 11:30:57 +0330

    dbtest

--- Pet_store/Shelter/urls.py
+++ Pet_store/Shelter/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Home ,animal
+from .views import Home ,animal 
 
 urlpatterns = [
     path('', Home, name='home'),
-    path('animal/<int:pet_id>' ,animal )
+    path('animal/<int:pet_id>' ,animal ),
+
 ]
