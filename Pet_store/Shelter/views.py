df33c13e2ece7cd3dbb43522161a073de49a6d38
ramin <ramindesu88@gmail.com>
2025-11-07 10:53:40 +0330

    New template

--- Pet_store/Shelter/views.py
+++ Pet_store/Shelter/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from .models import Pet
 
+
 # Create your views here.
 def Home(request):
-    pets = Pet.objects.all()
-    return render(request , 'Shelter/home.html' , {'pets' : pets})
+    pets = Pet.objects.filter(available = True)
+    return render(request, "Shelter/home.html", {"pets": pets})
+
+
+def animal(request, pet_id):
+    pet = Pet.objects.get(id = pet_id)
+    return render(request, 'Shelter/animal.html' , {'pet':pet})
