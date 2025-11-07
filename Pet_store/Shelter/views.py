0d12f7e2fee7fda0f7b789703986d5ee2b9a3535
ramin <ramindesu88@gmail.com>
2025-11-07 10:33:44 +0330

    ADD(urls,template,view)

--- Pet_store/Shelter/views.py
+++ Pet_store/Shelter/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Pet
 
 # Create your views here.
+def Home(request):
+    pets = Pet.objects.all()
+    render(request , 'home.html' , {'pets' : pets})
--- Pet_store/core/urls.py
+++ Pet_store/core/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from Shelter.views import Home
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/', Home)
 ]
