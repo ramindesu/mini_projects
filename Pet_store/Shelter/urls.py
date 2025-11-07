e5570488738096c0d9181e1c55a3a102bf68ef67
ramin <ramindesu88@gmail.com>
2025-11-07 11:01:48 +0330

    ADD(image)

--- Pet_store/Shelter/urls.py
+++ Pet_store/Shelter/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Home
+from .views import Home ,animal
 
 urlpatterns = [
     path('', Home, name='home'),
+    path('animal/<int:pet_id>' ,animal )
 ]
